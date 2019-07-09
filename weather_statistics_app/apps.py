2846803a46e15a5ace86076b6f93baa2e561f5ee
tanya525625 <tanya_postcrossing@mail.ru>
2019-07-09 17:32:21 +0300

    initial commit

---
+++ weather_statistics_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WeatherStatisticsAppConfig(AppConfig):
+    name = 'weather_statistics_app'
---
+++ weather_statistics_app/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from . import views
+from django.urls import include
+from django.contrib import admin
+from django.urls import path
+
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+]
