d6f052f21f5618ce3f03bb20c4ec169cbc82e5a0
Zubakhina Tatyana <33422677+tanya525625@users.noreply.github.com>
2020-04-20 20:20:39 +0300

    deploy test

--- weather_statistics/settings.py
+++ weather_statistics/settings.py
@@ -119,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'staticfiles')
+STATICFILES_DIRS = [os.path.join(os.path.dirname(BASE_DIR), 'weather_statistics', 'weather_statistics_app', 'static'), ]
+STATIC_URL = '/static/'
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
